ac4acff55bd5f91fd824701370c9ff4470a8e5f6
Kevin V <kevin.valette@gmail.com>
2018-05-07 10:35:05 +0200

    delete sendgrind + manifest ci + manifest link + dependency

--- src/configs/index.js
+++ src/configs/index.js
@@ -6,8 +6,4 @@ let firebaseConfig = {
   messagingSenderId: '404961037104'
 }
 
-let sendGrid = {
-  key: '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'
-}
-
-export { firebaseConfig, sendGrid }
+export { firebaseConfig }
