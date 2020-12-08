0eedb75167b4a41ae49589168e9f8d2ed1c00d2e
Kevin Valette <kevin.valette@gmail.com>
2020-12-08 12:37:59 +0100

    Update index.js

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,9 +1,9 @@
 let firebaseConfig = {
-  apiKey: 'AIzaSyA8W9WEhOKHbByfGqCBu_uEHK-sSmtZrG0',
-  authDomain: 'event-with.firebaseapp.com',
+  apiKey: '',
+  authDomain: '',
   databaseURL: 'https://event-with.firebaseio.com',
   storageBucket: 'event-with.appspot.com',
-  messagingSenderId: '404961037104'
+  messagingSenderId: ''
 }
 
 export { firebaseConfig }
